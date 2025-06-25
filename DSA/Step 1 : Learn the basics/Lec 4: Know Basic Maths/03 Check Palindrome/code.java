ccc0844afc498a32e8d1d5a741f2ca4845371064
parimalSavaj <parimalsavaj01@gmail.com>
2025-06-25 23:11:49 +0530

    Add palindrome checker program

---
+++ DSA/Step 1 : Learn the basics/Lec 4: Know Basic Maths/03 Check Palindrome/code.java
@@ -0,0 +1,19 @@
+import java.util.Scanner;
+
+public class code {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        System.out.print("Enter N value: ");
+        int num = sc.nextInt();
+
+        int duplicate = num;
+        int reverseNumber = 0;
+
+        while (num > 0) {
+            reverseNumber = (reverseNumber * 10) + num % 10;
+            num = num / 10;
+        }
+
+        System.out.println(duplicate == reverseNumber);
+    }
+}
