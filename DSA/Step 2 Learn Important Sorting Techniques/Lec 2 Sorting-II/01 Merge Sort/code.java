f7def267b652eefb1c8a843105790451add21fdc
parimal <parimalsavaj01@gmail.com>
2025-07-24 21:01:41 +0530

    Add Java implementation of Merge Sort algorithm with accompanying README detailing its functionality.

---
+++ DSA/Step 2 Learn Important Sorting Techniques/Lec 2 Sorting-II/01 Merge Sort/code.java
@@ -0,0 +1,48 @@
+public class code {
+
+    public static void merge(int[] arr, int low, int mid, int high) {
+        int[] temp = new int[high - low + 1];
+        int i = low;
+        int j = mid + 1;
+        int k = 0;
+
+        while (i <= mid && j <= high) {
+            if (arr[i] <= arr[j]) {
+                temp[k++] = arr[i++];
+            } else {
+                temp[k++] = arr[j++];
+            }
+        }
+
+        while (i <= mid) {
+            temp[k++] = arr[i++];
+        }
+        while (j <= high) {
+            temp[k++] = arr[j++];
+        }
+
+        for (int p = 0; p < temp.length; p++) {
+            arr[low + p] = temp[p];
+        }
+    }
+
+    public static void mergeSort(int[] arr, int low, int high) {
+        if (low >= high) {
+            return;
+        }
+        int mid = (low + high) / 2;
+        mergeSort(arr, low, mid);
+        mergeSort(arr, mid + 1, high);
+        merge(arr, low, mid, high);
+    }
+
+    public static void main(String[] args) {
+        int[] arr = { 5, 4, 3, 2, 1 };
+
+        mergeSort(arr, 0, arr.length - 1);
+
+        for (int i = 0; i < arr.length; i++) {
+            System.out.print(arr[i] + " ");
+        }
+    }
+}
