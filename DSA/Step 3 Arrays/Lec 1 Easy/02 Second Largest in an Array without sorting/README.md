76b7a8b27ab00f511c70163ccef031b900777a7e
parimal <parimalsavaj01@gmail.com>
2025-07-28 21:38:46 +0530

    Enhance Java implementation for finding the second largest element in an array by adding a condition to correctly identify the second largest value. Update README to reflect the new logic for determining the second largest element.

--- DSA/Step 3 Arrays/Lec 1 Easy/02 Second Largest in an Array without sorting/README.md
+++ DSA/Step 3 Arrays/Lec 1 Easy/02 Second Largest in an Array without sorting/README.md
@@ -19,6 +19,9 @@ for(int i = 0; i< arr.length; i++){
     if(arr[i] > largest){
         secondLargest = largest;
         largest = arr[i];
+    } 
+    if(arr[i] > secondLargest && arr[i] != largest){
+        secondLargest = arr[i];
     }
 }
 ```
--- DSA/Step 3 Arrays/Lec 1 Easy/02 Second Largest in an Array without sorting/code.java
+++ DSA/Step 3 Arrays/Lec 1 Easy/02 Second Largest in an Array without sorting/code.java
@@ -1,6 +1,6 @@
 public class code {
     public static void main(String[] args) {
-        int[] arr = { 1, 2, 3, 4, 5, 6, 7, 8, 9, 0 };
+        int[] arr = { 1, 2, 3, 4, 5, 6, 7, 9, 8 };
 
         int largest = arr[0];
         int secondLargest = -1;
@@ -9,6 +9,9 @@ public class code {
             if (arr[i] > largest) {
                 secondLargest = largest;
                 largest = arr[i];
+            }
+            if (arr[i] > secondLargest && arr[i] != largest) {
+                secondLargest = arr[i];
             }
         }
 
