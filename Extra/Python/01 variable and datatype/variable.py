796dcbb20049cd9962f1475adc65865d22ffeeea
parimal <parimalsavaj01@gmail.com>
2026-03-18 22:22:12 +0530

    feat: add variable and datatype examples in variable.py

---
+++ Extra/Python/01 variable and datatype/variable.py
@@ -0,0 +1,22 @@
+a = 10; # integer data-type
+b = 15.1; # float data-type
+c = "savaj"; # string data-type
+d = True; # boolean data-type
+e = None; # none data-type
+# f; you cannot declare a variable without assigning a value to it in the same statement
+
+# but we empty value assign to array, object and string.
+g = "";
+h = [];
+i = {};
+j = ''; # here have not any diff means inside python only string data-type, here not any data-type character.
+
+print(type(a))
+print(type(b))
+print(type(c))
+print(type(d))
+print(type(e))
+print(type(g))
+print(type(h))
+print(type(i))
+print(type(j))
---
+++ Extra/Python/first.py
@@ -0,0 +1,11 @@
+print("hello world") 
+# single line comments
+
+"""
+multi-line 
+comments
+"""
+
+# but we mostly use 
+# sigle line comments 
+# just here use
