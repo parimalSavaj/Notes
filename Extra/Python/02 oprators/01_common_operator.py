ecbbec6199b397939e4ca6008a735f200c30eaa2
parimal <parimalsavaj01@gmail.com>
2026-03-18 22:42:32 +0530

    feat: add common operators documentation in 01_common_operator.py

---
+++ Extra/Python/02 oprators/01_common_operator.py
@@ -0,0 +1,7 @@
+# arithmetic operator: +, -, *, /, % etc.
+
+# assignment operator: =, +=, -= etc.
+
+# comparison operator: ==, >, <, >=, <=, != etc. this operator only return boolean value.
+
+# logical operator: and or not. inside python logical operator write using keywords not symbols.
