3927dee979fc9a37e81eefe12ecb450cc05dd2b4
parimal <parimalsavaj01@gmail.com>
2026-03-20 22:39:08 +0530

    feat: add examples of for and while loops, including break, continue, and pass statements in Python

---
+++ Extra/Python/10 loop/03_list_use_in_loop.py
@@ -0,0 +1,11 @@
+arr = [1,"savaj", 15, "a",True]
+
+i =0;
+while(i<len(arr)):
+    print(arr[i])
+    i+=1;
+
+for i in arr:
+    print(i)
+
+# same list we can display tuple as well and "string"
---
+++ Extra/Python/10 loop/05_break_continue_pass.py
@@ -0,0 +1,18 @@
+# break
+
+for i in range(1,10):
+    print(i)
+    if(i>=5):
+        break;
+
+# continue
+
+for i in range(1,10):
+    if(i == 5):
+        continue
+    print(i)
+
+# pass
+
+for i in range(1):
+    pass
