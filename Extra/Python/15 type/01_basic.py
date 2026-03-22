6faeaa6d5941e08582c702b9f179299528b5169e
parimal <parimalsavaj01@gmail.com>
2026-03-22 23:12:02 +0530

    feat: add initial Python type definitions and a demo function

---
+++ Extra/Python/15 type/01_basic.py
@@ -0,0 +1,10 @@
+from typing import List,Dict
+
+# variable type define
+n : int = 5
+s : str = 5
+arr:List[int] = [1,2,3]
+obj:Dict[str, int] = {"age":18}
+
+def demo(i:int, j:str) -> bool:
+    return True
